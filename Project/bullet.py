01c131ce6fbb38c7e373d1f1748364cf352480b5
taejunji <121089600+taejunji@users.noreply.github.com>
2023-11-14 18:36:45 +0900

    Add BOss, bullet

---
+++ Project/bullet.py
@@ -0,0 +1,27 @@
+from pico2d import *
+import game_world
+import game_framework
+
+class Bullet:
+    image = None
+
+    def __init__(self, x = 400, y = 300, velocity = 1):
+        if Bullet.image == None:
+            Bullet.image = load_image('ball21x21.png')
+        self.x, self.y, self.velocity = x, y, velocity
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+    def update(self):
+        self.x += self.velocity * 100 * game_framework.frame_time
+
+        if self.x < 25 or self.x > 1600 - 25:
+            game_world.remove_object(self)
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def handle_collision(self,group, other):
+        if group == 'boss:bullet':
+            game_world.remove_object(self)
